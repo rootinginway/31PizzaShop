e4985a66ea043a2b98b08406a53eb282bc2c792f
rootinginway <romeopanasak@gmail.com>
2018-02-05 12:51:12 +0200

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,3 +17,19 @@ function something()
     	x = x * 1 + 1;
     	window.localStorage.setItem('product_' + id, x);
     }
+
+    function cart_get_number_of_items()
+    {
+    	var cnt = 0
+    	for(var i = 0; i < window.localStorage.length; i++)
+    	{
+    	   var key = window.localStorage.key(i);
+    	   //var value = window.localStorage.getItem(key);
+    	   if(key.indexOf('product_') == 0)
+    	   {
+    	   	cnt = cnt + value;
+    	   }
+
+    	} 
+    	return cnt;
+    }
