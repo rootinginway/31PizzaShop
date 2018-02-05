d955fc6c305e988cd00af33246b6453e5d78e5fb
rootinginway <romeopanasak@gmail.com>
2018-02-05 14:33:39 +0200

    Update orders input fields

--- public/app.js
+++ public/app.js
@@ -16,8 +16,14 @@ function something()
     	var x =  window.localStorage.getItem(key);
     	x = x * 1 + 1;
     	window.localStorage.setItem('product_' + id, x);
-    	alert('Items in your cart: ' + cart_get_number_of_items());
+    	update_orders_input();
     }
+
+function update_orders_input()
+{
+var orders = cart_get_orders();
+$('#orders_input').val(orders);  
+}
 
     
     function cart_get_number_of_items()
@@ -34,4 +40,20 @@ function something()
 
     	} 
     	return cnt * 1;
+    }
+
+     function cart_get_orders()
+    {
+    	var orders = '';
+    	for(var i = 0; i < window.localStorage.length; i++)
+    	{
+    	   var key = window.localStorage.key(i);
+    	   //var value = window.localStorage.getItem(key);
+    	   if(key.indexOf('product_') == 0)
+    	   {
+    	   	orders = orders + key + '=' + value + ',';
+    	   }
+
+    	} 
+    	return orders;
     }
